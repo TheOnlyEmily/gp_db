24df9f7a5416b3bafabfc9c2a832ce820632b9c0
Emily Ingram <emingram49@gmail.com>
2023-07-16 23:14:04 -0600

    updated to match example code

--- gp_db/database.py
+++ gp_db/database.py
@@ -1,3 +1,6 @@
 class SemanticTrackerDB:
-    def add_variable(self, name: str, semantics: list):
+    def __init__(self, semantics_length: int) -> None:
+        pass
+
+    def add_variable(self, name: str, semantics: list, semantics_type: type) -> int:
         return 0
--- tests/semantic_db_test.py
+++ tests/semantic_db_test.py
@@ -1,31 +1,40 @@
+from typing import Callable
 from hypothesis import given, strategies as st, assume
 from gp_db.database import SemanticTrackerDB
 
+@st.composite
+def semantics_and_type_pairs(draw: Callable) -> tuple[list, type]:
+    semantics_type = draw(st.sampled_from([bool, int, str]))
+    semantics_list = draw(st.lists(st.from_type(semantics_type), min_size=1))
+    return semantics_list, semantics_type
 
-def test_constructor_takes_no_arguments():
-    SemanticTrackerDB()
+@given(st.integers())
+def test_constructor_takes_one_argument(semantics_length):
+    SemanticTrackerDB(semantics_length)
 
 
 class TestAddVariableMethod:
     def test_add_variable_is_attribute(self):
-        db = SemanticTrackerDB()
+        db = SemanticTrackerDB(semantics_length=1)
         assert hasattr(db, "add_variable")
 
     def test_add_variable_is_callable(self):
-        db = SemanticTrackerDB()
+        db = SemanticTrackerDB(semantics_length=1)
         assert callable(db.add_variable)
 
-    @given(st.text(), st.lists(st.one_of(st.booleans(), st.integers(), st.text())))
-    def test_add_variable_takes_a_string_and_a_list(self, variable_name, semantic_list):
+    @given(st.text(), semantics_and_type_pairs())
+    def test_add_variable_takes_a_string_and_a_list(self, variable_name, semantic_list_and_type):
+        semantic_list, semantic_type = semantic_list_and_type
         assume(len(variable_name) > 0)
         assume(len(semantic_list) > 0)
-        db = SemanticTrackerDB()
-        db.add_variable(variable_name, semantic_list)
+        db = SemanticTrackerDB(semantics_length=1)
+        db.add_variable(variable_name, semantic_list, semantic_type)
 
-    @given(st.text(), st.lists(st.one_of(st.booleans(), st.integers(), st.text())))
-    def test_add_variable_returns_an_integer(self, variable_name, semantic_list):
+    @given(st.text(), semantics_and_type_pairs())
+    def test_add_variable_returns_an_integer(self, variable_name, semantic_list_and_type):
+        semantic_list, semantic_type = semantic_list_and_type
         assume(len(variable_name) > 0)
         assume(len(semantic_list) > 0)
-        db = SemanticTrackerDB()
-        semantics_id = db.add_variable(variable_name, semantic_list)
+        db = SemanticTrackerDB(semantics_length=1)
+        semantics_id = db.add_variable(variable_name, semantic_list, semantic_type)
         assert isinstance(semantics_id, int)
