0ef4702b35ef1be551c185be8ddeeef09a7471fc
Emily Ingram <lmingram@ualberta.ca>
2023-07-21 08:49:47 -0600

    new functionality and more flexible tests

--- gp_db/database.py
+++ gp_db/database.py
@@ -5,12 +5,13 @@ class SemanticTrackerDB:
         if semantics_length < 1:
             raise SemanticDbInitError("semantics length must be postive and non-zero")
         self._next_id: int = 0
+        self._semantics_length = semantics_length
 
     def add_variable(self, name: str, semantics: list, semantics_type: type) -> int:
         if name == '':
             raise SemanticEntryError("empty string is not a valid name")
-        if semantics == []:
-            raise SemanticEntryError("semantics cannot be an empty list")
+        if len(semantics) != self._semantics_length:
+            raise SemanticEntryError(f"expected semantics of length {self._semantics_length} not {len(semantics)}")
         semantics_id = self._next_id
         self._next_id += 1
         return semantics_id
--- tests/semantic_db_test.py
+++ tests/semantic_db_test.py
@@ -4,9 +4,12 @@ from hypothesis import given, strategies as st, assume
 from gp_db.database import SemanticTrackerDB, SemanticEntryError, SemanticDbInitError
 
 @st.composite
-def semantics_info(draw: Callable) -> tuple[list, type]:
+def semantics_info(draw: Callable, semantics_length=None) -> tuple[list, type]:
     semantics_type = draw(st.sampled_from([bool, int, str]))
-    semantics_list = draw(st.lists(st.from_type(semantics_type), min_size=1))
+    if semantics_length is not None:
+        semantics_list = draw(st.lists(st.from_type(semantics_type), min_size=semantics_length, max_size=semantics_length))
+    else:
+        semantics_list = draw(st.lists(st.from_type(semantics_type), min_size=1))
     variable_name = draw(st.text())
     return semantics_list, semantics_type, variable_name
 
@@ -39,49 +42,44 @@ class TestAddVariableMethod:
         db = SemanticTrackerDB(semantics_length=1)
         assert callable(db.add_variable)
 
-    @given(semantics_info())
+    @given(semantics_info(semantics_length=1))
     def test_add_variable_takes_a_string_and_a_list(self, semantics_info):
         semantic_list, semantic_type, variable_name = semantics_info
         assume(len(variable_name) > 0)
-        assume(len(semantic_list) > 0)
         db = SemanticTrackerDB(semantics_length=1)
         db.add_variable(variable_name, semantic_list, semantic_type)
 
-    @given(semantics_info())
+    @given(semantics_info(semantics_length=1))
     def test_add_variable_raises_semantic_entry_error_given_empty_string(self, semantics_info):
         semantic_list, semantic_type, _ = semantics_info
         variable_name = ''
-        assume(len(semantic_list) > 0)
         db = SemanticTrackerDB(semantics_length=1)
         with pytest.raises(SemanticEntryError, match="empty string is not a valid name"):
             db.add_variable(variable_name, semantic_list, semantic_type)
 
-    @given(semantics_info())
-    def test_add_variable_raises_semantic_entry_error_given_empty_list(self, semantics_info):
-        _, semantic_type, variable_name = semantics_info
+    @given(semantics_info(), st.integers(min_value=1))
+    def test_add_variable_raises_semantic_entry_error_given_list_of_wrong_size(self, semantics_info, expected_length):
+        semantic_list, semantic_type, variable_name = semantics_info
         assume(len(variable_name) > 0)
-        semantic_list = []
-        db = SemanticTrackerDB(semantics_length=1)
-        with pytest.raises(SemanticEntryError, match="semantics cannot be an empty list"):
+        assume(len(semantic_list) != expected_length)
+        db = SemanticTrackerDB(semantics_length=expected_length)
+        with pytest.raises(SemanticEntryError, match=f"expected semantics of length {expected_length} not {len(semantic_list)}"):
             db.add_variable(variable_name, semantic_list, semantic_type)
 
-    @given(semantics_info())
+    @given(semantics_info(semantics_length=1))
     def test_add_variable_returns_an_integer(self, semantics_info):
         semantic_list, semantic_type, variable_name = semantics_info
         assume(len(variable_name) > 0)
-        assume(len(semantic_list) > 0)
         db = SemanticTrackerDB(semantics_length=1)
         semantics_id = db.add_variable(variable_name, semantic_list, semantic_type)
         assert isinstance(semantics_id, int)
 
-    @given(semantics_info(), semantics_info())
+    @given(semantics_info(semantics_length=1), semantics_info(semantics_length=1))
     def test_add_variable_returns_a_different_integer_for_each_call(self, semantics_info1, semantics_info2):
         semantic_list1, semantic_type1, variable_name1 = semantics_info1
         semantic_list2, semantic_type2, variable_name2 = semantics_info2
         assume(len(variable_name1) > 0)
-        assume(len(semantic_list1) > 0)
         assume(len(variable_name2) > 0)
-        assume(len(semantic_list2) > 0)
         assume(semantic_list1 != semantic_list2)
         assume(variable_name1 != variable_name2)
         db = SemanticTrackerDB(semantics_length=1)
