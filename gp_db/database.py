298b3578de61d996da89c0a6579b2962d6a94036
Emily Ingram <lmingram@ualberta.ca>
2023-07-24 10:14:17 -0600

    improved readability and performance

--- gp_db/database.py
+++ gp_db/database.py
@@ -3,32 +3,19 @@ from typing import Callable
 
 class SemanticTrackerDB:
     def __init__(self, semantics_length: int) -> None:
-        if type(semantics_length) is not int:
-            raise SemanticDbInitError("semantics length must be of type int")
-        if semantics_length < 1:
-            raise SemanticDbInitError("semantics length must be postive and non-zero")
+        assert type(semantics_length) is int, "semantics length must be of type integer"
+        assert semantics_length > 0, "semantics length must be postive and non-zero"
         self._next_id: int = 0
         self._semantics_length = semantics_length
 
     def add_variable(self, semantics: list) -> int:
-        if not isinstance(semantics, list):
-            raise SemanticEntryError("semantics must be of type list")
-        if len(semantics) != self._semantics_length:
-            raise SemanticEntryError(f"expected semantics of length {self._semantics_length} not {len(semantics)}")
+        assert type(semantics) is list, "semantics must be of type list"
+        assert len(semantics) == self._semantics_length, f"expected semantics of length {self._semantics_length} not {len(semantics)}"
         semantics_id = self._next_id
         self._next_id += 1
         return semantics_id
 
     def add_function(self, name: str, function: Callable) -> int:
-        if not isinstance(name, str):
-            raise SemanticEntryError("function name must be of type string")
-        if name == '':
-            raise SemanticEntryError("empty string is not a valid name")
-        if not callable(function):
-            raise SemanticEntryError("function must be callable")
-
-
-class SemanticEntryError(Exception): ...
-
-
-class SemanticDbInitError(Exception): ...
+        assert type(name) is str, "name must be of type string"
+        assert name is not '', "empty string is not a valid name"
+        assert callable(function), "function must be callable"
--- tests/semantic_db_test.py
+++ tests/semantic_db_test.py
@@ -1,7 +1,7 @@
 import pytest
 from typing import Callable
 from hypothesis import given, strategies as st, assume
-from gp_db.database import SemanticTrackerDB, SemanticEntryError, SemanticDbInitError
+from gp_db.database import SemanticTrackerDB
 
 @st.composite
 def semantics_info(draw: Callable, semantics_length=None) -> tuple[list, type]:
@@ -20,15 +20,15 @@ class TestInitMethod:
         SemanticTrackerDB(semantics_length)
 
     @given(st.integers())
-    def test_constructor_raises_semantic_db_init_error_given_semantics_length_less_than_one(self, semantics_length):
+    def test_constructor_raises_assertion_error_init_error_given_semantics_length_less_than_one(self, semantics_length):
         assume(semantics_length <= 0)
 
-        with pytest.raises(SemanticDbInitError, match="semantics length must be postive and non-zero"): 
+        with pytest.raises(AssertionError, match="semantics length must be postive and non-zero"): 
             SemanticTrackerDB(semantics_length)
 
     @given(st.one_of(st.booleans(), st.text(), st.floats()))
-    def test_constructor_raises_semantic_db_init_error_given_semantics_length_not_of_type_int(self, semantics_length):
-        with pytest.raises(SemanticDbInitError, match="semantics length must be of type int"): 
+    def test_constructor_raises_assertion_error_given_semantics_length_not_of_type_int(self, semantics_length):
+        with pytest.raises(AssertionError, match="semantics length must be of type integer"): 
             SemanticTrackerDB(semantics_length)
 
 
@@ -47,16 +47,16 @@ class TestAddVariableMethod:
         db.add_variable(semantic_list)
 
     @given(semantic_list=st.one_of(st.booleans(), st.text(), st.floats()))
-    def test_add_variable_raises_semantic_entry_error_given_semantics_not_of_type_list(self, semantic_list):
-        with pytest.raises(SemanticEntryError, match="semantics must be of type list"):
+    def test_add_variable_raises_assertion_error_given_semantics_not_of_type_list(self, semantic_list):
+        with pytest.raises(AssertionError, match="semantics must be of type list"):
             db = SemanticTrackerDB(semantics_length=1)
             db.add_variable(semantic_list)
 
     @given(semantics_info(), st.integers(min_value=1))
-    def test_add_variable_raises_semantic_entry_error_given_list_of_wrong_size(self, semantic_list, expected_length):
+    def test_add_variable_raises_assertion_error_given_list_of_wrong_size(self, semantic_list, expected_length):
         assume(len(semantic_list) != expected_length)
         db = SemanticTrackerDB(semantics_length=expected_length)
-        with pytest.raises(SemanticEntryError, match=f"expected semantics of length {expected_length} not {len(semantic_list)}"):
+        with pytest.raises(AssertionError, match=f"expected semantics of length {expected_length} not {len(semantic_list)}"):
             db.add_variable(semantic_list)
 
     @given(semantics_info(semantics_length=1))
@@ -95,20 +95,20 @@ class TestAddFunctionMethod:
         assert db.add_function(func_name, func) is None
 
     @given(func=st.functions())
-    def test_add_function_raises_semantic_entry_error_given_empty_string(self, func):
+    def test_add_function_raises_assertion_error_given_empty_string(self, func):
         func_name = ''
         db = SemanticTrackerDB(semantics_length=1)
-        with pytest.raises(SemanticEntryError, match="empty string is not a valid name"):
+        with pytest.raises(AssertionError, match="empty string is not a valid name"):
             db.add_function(func_name, func)
 
     @given(function_name=st.one_of(st.booleans(), st.floats()), func=st.functions())
-    def test_add_function_raises_semantic_entry_error_given_function_name_not_of_type_string(self, function_name, func):
+    def test_add_function_raises_assertion_error_given_function_name_not_of_type_string(self, function_name, func):
         db = SemanticTrackerDB(semantics_length=1)
-        with pytest.raises(SemanticEntryError, match="function name must be of type string"):
+        with pytest.raises(AssertionError, match="name must be of type string"):
             db.add_function(function_name, func)
 
     @given(func_name=st.text(min_size=1), func=st.one_of(st.booleans(), st.text(), st.floats()))
-    def test_add_function_raises_semantic_entry_error_given_function_not_of_type_function(self, func_name, func):
+    def test_add_function_raises_assertion_error_given_function_not_of_type_function(self, func_name, func):
         db = SemanticTrackerDB(semantics_length=1)
-        with pytest.raises(SemanticEntryError, match="function must be callable"):
+        with pytest.raises(AssertionError, match="function must be callable"):
             db.add_function(func_name, func)
